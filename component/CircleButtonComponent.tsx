dc533f5f970ab68b5ee8820b29c59d67392f4661
Juan Guillermo Hernandez Alarcon <jghernande@bch.bancodechile.cl>
2023-09-09 12:05:47 -0500

    counter app

---
+++ component/CircleButtonComponent.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import Style from "@/styles/Home.module.css"
+
+
+interface CircleButtonProps {
+    buttonName: string;
+    action: () => void
+}
+
+const CircleButtonComponent = ({action, buttonName}: CircleButtonProps) => {
+
+  return (
+    <button className={Style.boton} onClick={action}>{buttonName}</button>
+  )
+}
+
+export default CircleButtonComponent
